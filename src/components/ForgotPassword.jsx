7dcebe21033dee1278141dccb85ba9c5fe9c6006
piyushNegiDev <piyushnegi381@gmail.com>
2026-06-03 11:53:59 +0530

    forgot password funtionality added

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -1,33 +1,93 @@
 import { useState } from "react";
 import { sendPasswordResetEmail } from "firebase/auth";
 import { auth } from "../config/firebase";
+import { ErrorMessage, Field, Formik, Form } from "formik";
+import { useNavigate } from "react-router-dom";
+import AnimatedButton from "./AnimatedButton";
+import * as Yup from "yup";
+
+const detailsSchemValidation = Yup.object().shape({
+  email: Yup.string().email("Invalid Email").required("Email is Required"),
+});
 
 const ForgotPassword = () => {
-  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
+  const handleSubmit = async ({ email }) => {
     try {
+      setLoading(true);
       await sendPasswordResetEmail(auth, email);
 
       alert("Password reset link sent");
     } catch (error) {
       alert(error.message);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="email"
-        placeholder="Enter your email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
+    <div className="max-w-220 mx-auto">
+      <h1 className="bg-surface p-3 text-center rounded-2xl font-semibold text-2xl mb-10 text-primary">
+        Forgot Password{" "}
+      </h1>
+      <div className="grid md:grid-cols-[1.2fr_0.7fr] items-center gap-10 bg-surface-secondary px-5 py-10 rounded-2xl">
+        <div>
+          <img className="rounded-xl" src="/notes-image.avif" alt="" />
+        </div>
+        <Formik
+          validationSchema={detailsSchemValidation}
+          initialValues={{
+            email: "",
+          }}
+          onSubmit={async (values) => {
+            await handleSubmit(values);
+          }}
+        >
+          <Form className="flex flex-col gap-4 text-text">
+            <div className="flex flex-col gap-1 relative">
+              <label htmlFor="email" className="text-lg">
+                Email
+              </label>
+              <Field
+                placeholder="example@gmail.com"
+                type="email"
+                name="email"
+                className="h-10 rounded-lg border px-3"
+              />
+              <div className="text-xs text-red-500 absolute -bottom-4">
+                <ErrorMessage name="email" />
+              </div>
+            </div>
 
-      <button type="submit">Send Reset Link</button>
-    </form>
+            <AnimatedButton
+              disabled={loading}
+              type={"submit"}
+              className={"rounded-lg border px-3 py-1.5 mt-5"}
+            >
+              {loading ? "Loading..." : "Forgot Password"}
+            </AnimatedButton>
+
+            <AnimatedButton
+              onClick={() => navigate("/login")}
+              type={"button"}
+              className={"rounded-lg border px-3 py-1.5"}
+            >
+              Log in
+            </AnimatedButton>
+
+            <AnimatedButton
+              type={"button"}
+              onClick={() => navigate("/")}
+              className={"rounded-lg border px-3 py-1.5"}
+            >
+              Create new account
+            </AnimatedButton>
+          </Form>
+        </Formik>
+      </div>
+    </div>
   );
 };
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,13 @@
 import { ErrorMessage, Field, Formik, Form } from "formik";
 import * as Yup from "yup";
-import {
-  sendPasswordResetEmail,
-  signInWithEmailAndPassword,
-  signOut,
-} from "firebase/auth";
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { toast } from "react-toastify";
 import { auth } from "../config/firebase";
 import AnimatedButton from "../components/AnimatedButton";
 
-const contactSchemValidation = Yup.object().shape({
+const detailsSchemValidation = Yup.object().shape({
   email: Yup.string().email("Invalid Email").required("Email is Required"),
   password: Yup.string()
     .required("Password is required")
@@ -71,7 +67,7 @@ const Login = () => {
           <img className="rounded-xl" src="/notes-image.avif" alt="" />
         </div>
         <Formik
-          validationSchema={contactSchemValidation}
+          validationSchema={detailsSchemValidation}
           initialValues={{
             email: "",
             password: "",
